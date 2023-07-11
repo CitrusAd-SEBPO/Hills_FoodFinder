9aabe0fbb93924f0c28a2554c4badcc100bfcbcf
CitrusAd-SEBPO <shahinur.islam@sebpo.com>
2023-07-11 06:58:16 +0600

    Create Active tab

--- tab.js
+++ tab.js
@@ -1,7 +1,22 @@
 
 
 // Tab JS
-function showContent(evt, itemName) {
+// function showContent(evt, itemName) {
+//     var i, search_content, bread_btn;
+//     search_content = document.getElementsByClassName("search_content");
+//     for (i = 0; i < search_content.length; i++) {
+//       search_content[i].style.display = "none";
+//     }
+//     bread_btn = document.getElementsByClassName("bread_btn");
+//     for (i = 0; i < bread_btn.length; i++) {
+//       bread_btn[i].className = bread_btn[i].className.replace(" active", "");
+//     }
+//     document.getElementById(itemName).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
+
+
+  function showContent(evt, itemName) {
     var i, search_content, bread_btn;
     search_content = document.getElementsByClassName("search_content");
     for (i = 0; i < search_content.length; i++) {
@@ -14,3 +29,6 @@ function showContent(evt, itemName) {
     document.getElementById(itemName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
